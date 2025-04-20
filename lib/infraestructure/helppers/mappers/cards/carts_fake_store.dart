7fd1c2f24ea231c33bf08c1c2a33abb6a02bf231
Mauricio <llanosmauricio10@gmail.com>
2025-04-20 13:59:05 -0500

    cart api response modified

--- lib/infraestructure/helppers/mappers/cards/carts_fake_store.dart
+++ lib/infraestructure/helppers/mappers/cards/carts_fake_store.dart
@@ -2,7 +2,7 @@
 class CartsFakeStore {
   final int id;
   final int userId;
-  final List<ProductsId> productsId;
+  final List<CartItem> productsId;
 
   CartsFakeStore({
     required this.id,
@@ -13,8 +13,8 @@ class CartsFakeStore {
   factory CartsFakeStore.fromJson(Map<String, dynamic> json) => CartsFakeStore(
     id: json["id"],
     userId: json["userId"],
-    productsId: List<ProductsId>.from(
-      json["products"].map((x) => ProductsId.fromJson(x)),
+    productsId: List<CartItem>.from(
+      json["products"].map((x) => CartItem.fromJson(x)),
     ),
   );
 
@@ -25,13 +25,13 @@ class CartsFakeStore {
   };
 }
 
-class ProductsId {
+class CartItem {
   final int productId;
 
-  ProductsId({required this.productId});
+  CartItem({required this.productId});
 
-  factory ProductsId.fromJson(Map<String, dynamic> json) =>
-      ProductsId(productId: json["productId"]);
+  factory CartItem.fromJson(Map<String, dynamic> json) =>
+      CartItem(productId: json["productId"]);
 
   Map<String, dynamic> toJson() => {"productId": productId};
 }
